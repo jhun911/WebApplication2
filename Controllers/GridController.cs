58847139bc87b294e310eee0759165bed371e328
jhun911 <67596230+jhun911@users.noreply.github.com>
2021-03-23 10:58:50 -0400

    Add files via upload

---
+++ Controllers/GridController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class GridController : Controller
+    {
+        private static ICollection<Product> products;
+        public GridController()
+        {
+            if (products == null)
+            {
+                var random = new Random();
+                products = Enumerable.Range(1, 100).Select(x => new Product
+                {
+                    Discontinued = x % 2 == 1,
+                    ProductID = x,
+                    ProductName = "Product " + x,
+                    UnitPrice = random.Next(1, 1000),
+                    UnitsInStock = random.Next(1, 1000),
+                    UnitsOnOrder = random.Next(1, 1000)
+
+                }).ToList();
+            }
+        }
+
+        public IActionResult CustomDataSource()
+        {
+            return View();
+        }
+        public ActionResult Read()
+        {
+            return Json(new MyResponseModel { DataCollection = products, TotalRecords = products.Count });
+        }
+    }
+}
---
+++ Models/MyResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class MyResponseModel
+    {
+        public IEnumerable DataCollection { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
