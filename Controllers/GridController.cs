34afc9f8f5a226f2319a447ed8392ac03c5b9832
qilin <qilin@umd.edu>
2021-05-04 21:15:35 -0400

    Price Test 5

--- Controllers/GridController.cs
+++ Controllers/GridController.cs
@@ -22,9 +22,9 @@ namespace WebApplication2.Controllers
                     Discontinued = x % 2 == 1,
                     ProductID = x,
                     ProductName = "Product " + x,
-                    UnitPrice = random.Next(10, 200),
-                    UnitsInStock = random.Next(10, 200),
-                    UnitsOnOrder = random.Next(10, 200)
+                    UnitPrice = random.Next(10, 300),
+                    UnitsInStock = random.Next(10, 300),
+                    UnitsOnOrder = random.Next(10, 300)
 
                 }).ToList();
             }
