e59f205ef6d2653ea14f49e67461e7acfc44cbc4
jhun911 <qilink@gmail.com>
2021-05-30 14:59:03 -0400

    Test 258

--- Controllers/GridController.cs
+++ Controllers/GridController.cs
@@ -22,9 +22,9 @@ namespace WebApplication2.Controllers
                     Discontinued = x % 2 == 1,
                     ProductID = x,
                     ProductName = "Product " + x,
-                    UnitPrice = random.Next(11, 300),
-                    UnitsInStock = random.Next(11, 300),
-                    UnitsOnOrder = random.Next(11, 300)
+                    UnitPrice = random.Next(12, 300),
+                    UnitsInStock = random.Next(12, 300),
+                    UnitsOnOrder = random.Next(12, 300)
 
                 }).ToList();
             }
